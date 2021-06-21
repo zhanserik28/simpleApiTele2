05a87f5ac7d1612353c3e6ce233ce8799c7f59d0
Zhanserik Bolat <bolat.zhanserik@gmail.com>
2021-06-21 20:09:50 +0600

    ok now should work

--- src/main/java/org/example/simpleApi/dao/UserInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.simpleApi.dao;
-
-import org.example.simpleApi.model.UserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserInfoRepository  extends JpaRepository<UserInfo, Long> {
-
-}
